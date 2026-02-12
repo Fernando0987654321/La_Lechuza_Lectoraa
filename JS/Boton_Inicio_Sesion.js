21d85893449be81801bdfb9019cb7a6a23ab842c
Fernando0987654321 <l231080092@iztapalapa.tecnm.mx>
2026-02-12 19:16:25 +0000

    cambios30

--- JS/Boton_Inicio_Sesion.js
+++ JS/Boton_Inicio_Sesion.js
@@ -1,16 +1,24 @@
-
 document.addEventListener('DOMContentLoaded', () => {
 
     const headerLoginButtons = document.querySelectorAll('.btn-primary');
 
-    const userLoginURL = 'html/Inicio_de_sesion/Inicio_sesion.html';
+    const userLoginURL = '/html/Inicio_de_sesion/Inicio_sesion.html';
 
     headerLoginButtons.forEach(button => {
+        
         if (button.textContent.trim() === 'Iniciar Sesión') {
             button.addEventListener('click', () => {
-
+                
                 window.location.href = userLoginURL;
             });
         }
     });
+
+  
+    const loginIconBtn = document.getElementById('btn-login-redirect');
+    if (loginIconBtn) {
+        loginIconBtn.onclick = () => {
+            window.location.href = userLoginURL;
+        };
+    }
 });
